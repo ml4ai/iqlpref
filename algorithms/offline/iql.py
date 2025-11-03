3018fc9994a173eb15c80d33ad50e8f018edd09f
Loren Champlin <champlin@email.arizona.edu>
2025-11-03 10:19:22 -0700

    fixed

--- algorithms/offline/iql.py
+++ algorithms/offline/iql.py
@@ -246,10 +246,10 @@ def return_reward_range(dataset, max_episode_steps):
     ep_ret, ep_len = 0.0, 0
     i = 0
     trj_lens = np.zeros(dataset["rewards"].shape[0])
-    for j, r, d in enumerate(zip(dataset["rewards"], dataset["terminals"])):
+    for j, (r, d) in enumerate(zip(dataset["rewards"], dataset["terminals"])):
         ep_ret += float(r)
         ep_len += 1
-        trj_lens[i:j+1] = ep_len
+        trj_lens[i : j + 1] = ep_len
         if d or ep_len == max_episode_steps:
             i = j + 1
             returns.append(ep_ret)
@@ -262,13 +262,13 @@ def return_reward_range(dataset, max_episode_steps):
 
 def modify_reward(dataset, env_name, min_max_normalize_rwd, max_episode_steps=1000):
     if any(s in env_name for s in ("halfcheetah", "hopper", "walker2d")):
-        min_ret, max_ret,_ = return_reward_range(dataset, max_episode_steps)
+        min_ret, max_ret, _ = return_reward_range(dataset, max_episode_steps)
         dataset["rewards"] /= max_ret - min_ret
         dataset["rewards"] *= max_episode_steps
     elif "antmaze" in env_name:
         if min_max_normalize_rwd:
             min_ret, max_ret, trj_lens = return_reward_range(dataset, max_episode_steps)
-            dataset["rewards"] -= (min_ret / trj_lens)
+            dataset["rewards"] -= min_ret / trj_lens
             dataset["rewards"] /= max_ret - min_ret
             dataset["rewards"] *= max_episode_steps
         dataset["rewards"] -= 1.0
