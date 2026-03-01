e56e5c31640a14754b4339267dec545cd4c45b64
Loren Champlin <champlin@email.arizona.edu>
2026-02-28 22:02:52 -0700

    update

--- get_map.py
+++ get_map.py
@@ -5,22 +5,24 @@ sys.path.insert(0, os.path.abspath("gp_reward-priors"))
 sys.path.insert(0, os.path.abspath("../"))
 from optbnn.bnn.likelihoods import LikCE
 from optbnn.bnn.nets.mlp import MLP
-from optbnn.bnn.priors import FixedGaussianPrior
+from optbnn.bnn.priors import OptimGaussianPrior
 from optbnn.sgmcmc_bayes_net.pref_net import PrefNet
 from optbnn.utils import util
 
-util.set_seed(5)
+util.set_seed(3)
 # Initialize BNN Priors
-width = 128  # Number of units in each hidden layer
-depth = 4  # Number of hidden layers
+width = 256  # Number of units in each hidden layer
+depth = 3  # Number of hidden layers
 transfer_fn = "relu"  # Activation function
 
 X_train, y_train, _, _ = util.load_pref_data(
-    "./gp_reward-priors/data/antmaze/antmaze-large-diverse-v2_pref.hdf5", 0.1
+    "./gp_reward-priors/data/antmaze/antmaze-medium-play-v2_pref.hdf5", 0.5
 )
 
+prior_dir = "./gp_reward-priors/exp/reward_learning/antmaze_tuning/br-antmaze_medium_play-256-3/ckpts/it-1000.ckpt"
+
 # Initialize the prior
-prior = FixedGaussianPrior(std=1.0)
+prior = OptimGaussianPrior(prior_dir)
 
 # Setup likelihood
 net = MLP(37, 1, [width] * depth, transfer_fn)
@@ -28,11 +30,11 @@ likelihood = LikCE()
 
 # Initialize the sampler
 saved_dir = os.path.join(
-    "./antmaze_models/antmaze_br/FG/antmaze-large-diverse-v2/reduce_90_br",
-    "sampling_std",
+    "./antmaze_models/antmaze_br/optim/antmaze-medium-play-v2/reduce_50_br",
+    "sampling_optim",
 )
 util.ensure_dir(saved_dir)
-bayes_net_std = PrefNet(net, likelihood, prior, saved_dir, n_gpu=1, name="FG")
+bayes_net_std = PrefNet(net, likelihood, prior, saved_dir, n_gpu=1, name="optim")
 
 bayes_net_std.sampled_weights = bayes_net_std._load_sampled_weights(
     os.path.join(saved_dir, "sampled_weights", "sampled_weights_0000003")
